2a2c59228bcfb9fdd4108250376060f01d7507ca
suhkadam <suhkadam@DIN59016253.corp.capgemini.com>
2020-05-27 17:16:46 +0530

    Added constant .java

---
+++ Constant.java
@@ -0,0 +1,70 @@
+
+// Utility File - Constants
+
+package com.test.subway.utility;
+
+/**
+ * @author shashank.a.bansal
+ */
+public class Constant {
+	public static final String URL = "https://qe-order.remote-order.com/LoyaltyPilot.aspx?";
+
+	public static final String Path_TestData = System.getProperty("user.dir") + "/src/com/test/subway/testdata/";
+	public static final String File_TestData = "Mobile_TestData.xlsx";
+
+	public static final String sheetName = "Sheet2";
+	public static final String sheet = "Sheet4";
+	public static final String Sheet_Regression = "Regression Suite";
+	public static final String Sheet_Sanity = "Sanity Suite";
+
+	// Credit card details
+	public static final String CardHolder_Name = "Test1";
+	public static final String CC_Num = "4111111111111111";
+
+	public static final String CC_ExpMM = "12";
+	public static final String CC_ExpYYYY = "2025";
+	public static final String CC_Code = "1234";
+
+	// Test Data Sheet Columns
+	public static final int Col_TestCaseName = 0;
+	public static final int Col_UserName = 1;
+	public static final int Col_Password = 2;
+	public static final int Col_Browser = 3;
+	public static final int Col_FirstName = 4;
+	public static final int Col_LastName = 5;
+	public static final int Col_Country = 6;
+	public static final int Col_Phone = 7;
+	public static final int Col_Mobile = 8;
+	public static final int Col_SelectStoreZip = 9;
+	public static final int Col_PostCode = 10;
+	public static final int Col_PreferedLanguage = 11;
+	public static final int col_product = 12;
+	public static final int Col_Type = 13;
+	public static final int col_productselector = 14;
+	public static final int Col_Extra = 15;
+	public static final int Col_Quantity = 16;
+	public static final int Col_Meal = 17;
+	public static final int Col_AddMoreItems = 18;
+	public static final int Col_Sides = 19;
+	public static final int Col_Drinks = 20;
+	public static final int Col_ApplyOffer = 21;
+	public static final int Col_OfferCode = 22;
+	public static final int Col_LoyaltyCert = 23;
+	public static final int Col_Card = 24;
+	public static final int Col_Result = 25;
+	public static final int col_comment = 26;
+	public static final int col_cvv = 27;
+	public static final int col_billing_address = 28;
+	public static final int col_payal_email = 29;
+	public static final int col_paypal_password = 30;
+	public static final int col_paymentcard = 31;
+	public static final int col_cardNumberTwo = 32;
+	public static final int Col_Pin1 = 33;
+	public static final int Col_Pin2 = 34;
+	public static final int col_cardNumberThree = 35;
+	public static final int Col_Pin3 = 36;
+	public static final int Col_Zipcodetwo = 37;
+
+	public static final String Path_ScreenShot = System.getProperty("user.dir") + "/Screenshots/";
+
+}
